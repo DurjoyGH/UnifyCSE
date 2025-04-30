a61d0453c057feacb8dcb94a4a20214a45faae99
DurjoyGH <durjoyghosh328@gmail.com>
2025-05-01 01:53:40 +0600

    folder structure setup of frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RouterConfig from "./Routes";
+
+function App() {
+  return (
+    <div>
+      <RouterConfig />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const RouterConfig = () => {
+    return (
+        <Router>
+            <Routes>
+
+            </Routes>
+        </Router>
+    )
+}
+
+export default RouterConfig;
